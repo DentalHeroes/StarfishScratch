8f054ee8f94963b1b42a892af79e811114473baa
Tyler Fields <tyler.fields777@gmail.com>
2018-06-27 19:13:14 -0400

    Refactor JS to class. Get user location and grab weather.

--- templates/mainWeather.js
+++ templates/mainWeather.js
@@ -1,31 +1,57 @@
-var mainWeather = {
-      init : function(){
-          $("#submitWeather").click(function () {
-                    return mainJS.getWeather();
-            }
-        });
-      },
- 
-    getWeather: function () {
+class mainWeather {
+    constructor() {
+        this.city;
+        this.state;
+        this.zip;
+
+        this.showPosition = this.showPosition.bind(this);
+        this.getWeather = this.getWeather.bind(this);
+        this.getLocation();
+    }
+
+    getLocation() {
+        if (navigator.geolocation) {
+            navigator.geolocation.getCurrentPosition(this.showPosition);
+        }
+    }
+
+    showPosition(position) {
         $.ajax({
-            url: 'http://api.openweathermap.org/data/2.5/weather?q=' + $("#cityInput").val() + "," + $("#countryInput").val(),
+            url: 'https://maps.googleapis.com/maps/api/geocode/json?latlng=' + position.coords.latitude + ','+ position.coords.longitude + '&sensor=true',
+            type: 'get',
+            success: function (data) {
+                var addressArray = data.results[5].formatted_address.split(', ');
+                this.city = addressArray[0];
+                this.state = addressArray[1];
+                this.zip = data.results[2].formatted_address.split(', ')[1].slice(3);
+                this.getWeather();
+            }.bind(this),
+            error: function (error) {
+                alert("We're sorry, there seems to be an error, please try again")
+            }
+        })
+    }
+
+    getWeather() {
+        debugger
+        $.ajax({
+            url: 'http://api.openweathermap.org/data/2.5/weather?zip=' + this.zip + ',us' + "&appid=3c90ef0d527512b433ebf00ac0321e72",
             type: 'GET',
             dataType: 'jsonp',
             success: function (data) {
                 var wrapper = $("#WeatherWrapper");
                 wrapper.empty();
-                wrapper.append("<div class='city'> <p>Place: " + data.name + ", " + data.sys.country + "</p></div>");
-                wrapper.append(mainWeather.createWeatherWidg(data));
-            },
-            error: function () {
+                wrapper.append("<div class='city'> <p>Place: " + this.city + ", " + this.state + "</p></div>");
+                wrapper.append(this.createWeatherWidg(data));
+            }.bind(this),
+            error: function (error) {
                 alert('Failed!');
             }
- 
+
         });
-    },
- 
-      //Prints result from the weatherapi, receiving as param an object
-       createWeatherWidg: function (data) {
+    }
+
+    createWeatherWidg(data) {
         return "<div class='pressure'> <p>Temperature: " + (data.main.temp - 273.15).toFixed(2) + " C</p></div>"+
                 "<div class='description'> <p>Title: " + data.weather[0].main + "</p></div>" +
                 "<div class='description'> <p>Description: " + data.weather[0].description + "</p></div>" +
