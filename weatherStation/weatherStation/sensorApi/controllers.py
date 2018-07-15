525f1cdeb610de11a6c8353970eb92250e50c81b
Will Walter <wilwa74@gmail.com>
2018-07-15 18:33:21 +0000

    Finalized dht11 code

--- weatherStation/weatherStation/sensorApi/controllers.py
+++ weatherStation/weatherStation/sensorApi/controllers.py
@@ -3,22 +3,27 @@ from flask import jsonify
 import dht11
 
 class SensorData():
-    def __init__(self, temp=0, humidity=0):
+    def __init__(self, errorCode=0, temp=0, humidity=0):
+        self.errorCode = errorCode
         self.temp = temp
         self.humidity = humidity
     
     def serialize(self):
-        return {'temp' : self.temp, 'humidity' : self.humidity }
+        return {'errorCode' : self.errorCode, 'temp' : self.temp, 'humidity' : self.humidity }
 
 sensorApi = Blueprint('sensorApi', __name__)
 
 @sensorApi.route('/')
 def index():
-    result = dht11.getData()
+    dth11Object = dht11.DTH11(14)
+    result = dth11Object.read()
     if result:
-        humidity, temperature = result
+        errorCode = result.errorCode
+        humidity = result.humidity
+        temperature = result.temperature
     else:
+        errorCode = 0
         humidity = 0
         temperature = 0
-    result = SensorData(temperature, humidity)
+    result = SensorData(errorCode, temperature, humidity)
     return jsonify(result.serialize())
