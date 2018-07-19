c3b13cf8fc3447b90b02bfa1acb5a5e29f2efa48
GWillikerz <billgerlib@gmail.com>
2018-07-19 03:51:04 +0000

    added sensor loop

--- weatherStation/weatherStation/sensorApi/controllers.py
+++ weatherStation/weatherStation/sensorApi/controllers.py
@@ -1,6 +1,7 @@
 from flask import Blueprint
 from flask import jsonify
 import dht11
+import time
 
 class SensorData():
     def __init__(self, errorCode=0, temp=0, humidity=0):
@@ -15,15 +16,21 @@ sensorApi = Blueprint('sensorApi', __name__)
 
 @sensorApi.route('/')
 def index():
-    dth11Object = dht11.DTH11(8)
-    result = dth11Object.read()
-    if result:
-        errorCode = result.errorCode
-        humidity = result.humidity
-        temperature = result.temperature
-    else:
-        errorCode = 0
-        humidity = 0
-        temperature = 0
+    dth11Object = dht11.DTH11(8)	
+    for x in range(20):
+		result = dth11Object.read()
+		if result:
+			errorCode = result.errorCode
+			humidity = result.humidity
+			temperature = result.temperature
+			#if there are no read errors, then break out of loop
+			if result.errorCode == 0:
+				break
+			else:
+				time.sleep(.5)
+		else:
+			errorCode = 0
+			humidity = 0
+			temperature = 0
     result = SensorData(errorCode, temperature, humidity)
     return jsonify(result.serialize())
